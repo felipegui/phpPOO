3ce48863fd894e281666061568a911315770bf55
Felipe Guimaraes <fguiengenharia@gmail.com>
2021-11-16 10:23:07 -0300

    Método construtor em uma classe

--- index.php
+++ index.php
@@ -1,26 +1,24 @@
 <?php
 
-//A classe contém as propriedades do objeto que eu estou querendo criar
 class Post {
-    //Usando a palavra "public" para poder ter acesso as propriedades fora da classe
-    public int $likes = 0;//Typed properties (7.4)
-    public array $comments = [];//Typed properties (7.4)
-    public string $author;//Typed properties (7.4)
+    
+    public int $likes = 0;
+    public array $comments = [];
+    public string $author;
+
+    //Criando o método construtor
+    public function __construct($qtLikes) {
+        $this->likes = $qtLikes;
+    }
 
     public function aumentarLikes(){
         $this->likes++;
     }
 }
 
-//criando o objeto a partir da classe
-$post1 = new Post();
-//acessando as propriedades da classe Post
-$post1->likes = 3;
-$post1->author = "Felipe";
-
-$post2 = new Post();
-$post2->likes = 10;
+$post1 = new Post(25);
+$post2 = new Post(0);
 
 echo "Post 1: ".$post1->likes."</br>";
-echo "Post 2: ".$post1->author;
+echo "Post 2: ".$post2->likes;
 ?>
